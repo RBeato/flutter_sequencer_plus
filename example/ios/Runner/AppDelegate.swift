2d07ca240c017258bd04f3eb74416e6e11063939
Romeu Beato <romeufbeato@gmail.com>
2025-04-26 14:56:25 +0100

    ios builds

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import flutter_sequencer
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,10 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    // Manual plugin registration in Swift
+    let registry = self.registrar(forPlugin: "FlutterSequencerPlugin")
+    SwiftFlutterSequencerPlugin.register(with: registry!)
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
